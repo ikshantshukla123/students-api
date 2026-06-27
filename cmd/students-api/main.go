c479c535150e96111fed7a0039380a3516452db3
ikshantshukla123 <ikshankshukla44@gmail.com>
2026-06-27 12:06:00 +0530

    graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,10 +1,72 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-import "fmt"
+	"github.com/ikshantshukla123/students-api/internal/config"
+)
 
 
 func main(){
-	fmt.Println("Hello, Students API!")
+	//load config
+	cfg := config.MustLoad()
+	//database setup
+	//setup router
+	 router := http.NewServeMux()
+	 router.HandleFunc("GET /home",func(w http.ResponseWriter,r *http.Request){
+		w.Write([]byte("welcome to students api"))
+	 })
+
+	 
+	//setup server
+
+
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+
+done := make(chan os.Signal,1)
+signal.Notify(done,os.Interrupt,syscall.SIGINT)
+
+go func (){
+	slog.Info("server started",slog.String("address",cfg.Addr))
+	fmt.Printf("Server started %v", cfg.Addr)
+	err:= server.ListenAndServe()
+	if err != nil{
+	log.Fatal("Failed to start server")
+	}
+}()
+<-done
+
+slog.Info("Shutting down the server")
+
+
+ctx,cancel := context.WithTimeout(context.Background(),5 * time.Second)
+
+defer cancel()
+
+err := server.Shutdown(ctx)
+if err != nil{
+	slog.Error("Failed to shutdown server",slog.String("error",err.Error()))
 }
+slog.Info("Server shutdown successfully")
+}
+
  
+//use logger in place of println/print : log/slog
+// Because logs are not just for printing messages. They help you:
+
+// Debug errors
+// Monitor servers
+// Know what users are doing
+// Find why your API crashed
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,19 +2,19 @@ package config
 
 import (
 	"flag"
-	"os"
-	"log"
+	"os" //used to communicate with operating system
+	"log" //used to print log
  "github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env:"HTTP_ADDRESS" env-default:"localhost:8080"`
 }
 
 type Config struct {
 	Env         string `yaml:"env" env:"ENV" env-default:"production"`
 	StoragePath string `yaml:"storage_path" env-default:"storage/storage.db" env-required:"true"`
-	HTTPServer `yaml:"http_server"`
+	HTTPServer `yaml:"http_server"` //embedding is done here 
 }
 
 func MustLoad() *Config{
@@ -23,17 +23,18 @@ func MustLoad() *Config{
 	configPath = os.Getenv("CONFIG_PATH")
 
 	if configPath == ""{
+		//flags : Used to read command-line arguments.
 		flags := flag.String("config","","path to configuration file")
-		flag.Parse()
+		flag.Parse() //Go  reads command-line arguments with this .
 		
-		configPath = *flags
+		configPath = *flags //flag.string return pointer 
 
 		if configPath == ""{
-			log.Fatal("Config path is not set")
+			log.Fatal("Config path is not set") //fatal print message and immediately stops program
 		}
 	}
 
-	if _,err := os.Stat(configPath); os.IsNotExist(err){
+	if _,err := os.Stat(configPath); os.IsNotExist(err){ //Does this file exist and if not then give error 
 		log.Fatalf("Config file does not exist: %s",configPath)
 	}
 
